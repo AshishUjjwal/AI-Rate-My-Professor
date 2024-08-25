a43a3abefd99c7b0204507aa52158c694bd0f5ac
Ashish Ujjwal <98599848+AshishUjjwal@users.noreply.github.com>
2024-08-25 20:41:00 +0530

    intial commit

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI-Rate-My-PRofessor",
+  description: "It is AI which help to rate professor",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
